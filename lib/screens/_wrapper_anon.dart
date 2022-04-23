ba74fb910b41720e149736add87c8e7fc7dadd2a
ousstachdz <oussama.tachi@outlook.com>
2022-04-23 12:17:12 +0100

    work on login

---
+++ lib/screens/_wrapper_anon.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'account/index.dart';
+import 'rbac/anon.dart';
+
+class AnonWrapper extends StatefulWidget {
+  @override
+  _AnonWrapperState createState() => _AnonWrapperState();
+}
+
+class _AnonWrapperState extends State<AnonWrapper> {
+  int _selectedPage = 0;
+  final _pageOptions = [
+    AnonScreen(),
+    AccountScreen(),
+    AccountScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pageOptions[_selectedPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPage,
+        showUnselectedLabels: false,
+        onTap: (int index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.masks),
+            label: 'Anonymous',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.help),
+            label: 'Help',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
